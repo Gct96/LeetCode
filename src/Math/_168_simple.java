01b55b7c81c0a2f8a6d7640f319d05e5697f5d9c
Chongtao Guo <sxchongtao@163.com>
2022-05-06 20:59:45 +0800

    2022.5.6

--- src/Math/_168_simple.java
+++ src/Math/_168_simple.java
@@ -1,12 +1,31 @@
 package Math;
 
 /**
+ * 给你一个整数 columnNumber ，返回它在 Excel 表中相对应的列名称
+ * 例如
+ *  1 -> A
+ *  2 -> B
+ *  3 -> C
+ * 26 -> Z
+ * 27 -> AA
+ * 28 -> AB
  * @author keyboardhero
  * @create 2022-05-05 21:01
  */
 public class _168_simple {
     public String convertToTitle(int columnNumber){
         StringBuilder sb = new StringBuilder();
+        while(columnNumber>0){
+            columnNumber--;
+            sb.append((char)(columnNumber%26+'A'));
+            columnNumber/=26;
+        }
+        sb.reverse();
+        return sb.toString();
+    }
+
+    public String convertToTitle_rev1(int columnNumber){
+        StringBuilder sb=new StringBuilder();
         while(columnNumber>0){
             columnNumber--;
             sb.append((char)(columnNumber%26+'A'));
---
+++ src/Math/_171_simple.java
@@ -0,0 +1,23 @@
+package Math;
+
+/**
+ * 给你一个字符串 columnTitle表示Excel表格中的列名称。返回该列名称对应的列序号。
+ * A -> 1
+ * B -> 2
+ * C -> 3
+ * Z -> 26
+ * AA -> 27
+ * AB -> 28
+ * @author keyboardhero
+ * @create 2022-05-06 20:47
+ */
+public class _171_simple {
+    public int titleToNumber(String columnTitle){
+        int ans=0;
+        for(int i=0;i<columnTitle.length();i++){
+            int num=columnTitle.charAt(i)-'A'+1;
+            ans=ans*26+num;
+        }
+        return ans;
+    }
+}
