5c49bea5f425eb4a3e1a9ea8bb7aea7486905757
Chongtao Guo <sxchongtao@163.com>
2022-05-25 15:13:58 +0800

    2022.5.25

--- src/Math/_883_simple.java
+++ src/Math/_883_simple.java
@@ -25,4 +25,26 @@ public class _883_simple {
         }
         return xyArea+xzArea+yzArea;
     }
+    /**
+     *  * 在n x n的网格grid中，我们放置了一些与 x，y，z 三轴对齐的1 x 1 x 1立方体。
+     *  * 每个值v = grid[i][j]表示 v个正方体叠放在单元格(i, j)上。
+     *  * 现在我们查看这些立方体在 xy、yz和 zx平面上的投影。
+     *  * 投影就像影子，将三维形体映射到一个二维平面上。从顶部、前面和侧面看立方体时，我们会看到“影子”。
+     *  * 返回所有三个投影的总面积
+     */
+    public int projectionArea_rev1(int[][] grid){
+        int n=grid.length;
+        int xyArea=0,xzArea=0,yzArea=0;
+        for(int i=0;i<n;i++){
+            int xzHeight=0,yzHeight=0;
+            for(int j=0;j<n;j++){
+                xyArea+=grid[i][j]>0?1:0;
+                xzHeight=Math.max(xzHeight,grid[j][i]);
+                yzHeight=Math.max(yzHeight,grid[i][j]);
+            }
+            xzArea+=xzHeight;
+            yzArea+=yzHeight;
+        }
+        return xyArea+xzArea+yzArea;
+    }
 }
